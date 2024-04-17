ec921feabf4ec6e05dab54588b7c8dd355def70f
00leksandr <138134482+00leksandr@users.noreply.github.com>
2024-04-17 14:03:45 +0300

    Add files via upload

---
+++ ПР15/ПР15/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ПР15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.bookNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bookAuthorTextBox = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(500, 616);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // bookNameTextBox
+            // 
+            this.bookNameTextBox.Location = new System.Drawing.Point(518, 59);
+            this.bookNameTextBox.Multiline = true;
+            this.bookNameTextBox.Name = "bookNameTextBox";
+            this.bookNameTextBox.Size = new System.Drawing.Size(217, 22);
+            this.bookNameTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(518, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введіть назву прочитаної книги";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(518, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введіть автора прочитаної книги";
+            // 
+            // bookAuthorTextBox
+            // 
+            this.bookAuthorTextBox.Location = new System.Drawing.Point(518, 123);
+            this.bookAuthorTextBox.Multiline = true;
+            this.bookAuthorTextBox.Name = "bookAuthorTextBox";
+            this.bookAuthorTextBox.Size = new System.Drawing.Size(217, 22);
+            this.bookAuthorTextBox.TabIndex = 3;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(748, 59);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(94, 86);
+            this.Add.TabIndex = 5;
+            this.Add.Text = "добавити";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Завантажити список книг";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 668);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bookAuthorTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bookNameTextBox);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox bookNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox bookAuthorTextBox;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ПР15/ПР15/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ПР15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            listView1.Columns.Add("Назва книги", 200);
+            listView1.Columns.Add("Автор", 200);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string bookName = bookNameTextBox.Text;
+            string bookAuthor = bookAuthorTextBox.Text;
+
+            ListViewItem item = new ListViewItem(new string[] { bookName, bookAuthor });
+            listView1.Items.Add(item);
+
+            using (StreamWriter writer = new StreamWriter("books.txt", true))
+            {
+                writer.WriteLine($"{bookName},{bookAuthor}");
+            }
+
+            bookNameTextBox.Text = "";
+            bookAuthorTextBox.Text = "";
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            using (StreamReader reader = new StreamReader("books.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    ListViewItem item = new ListViewItem(new string[] { parts[0], parts[1] });
+                    listView1.Items.Add(item);
+                }
+            }
+        }
+    }
+}
